5111ba5ce9b76d3f3edf59f51784791ceabe3919
sh1928kd <sh1928kd@sh1928kds-MacBook-Pro.local>
2020-05-07 00:58:04 +0900

    :hammer: FizzBuzzStyleクラスを実装

--- src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzStyle.cs
+++ src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzStyle.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using sh1928kd.FizzBuzzProfessionalEdition.Model;
 
 namespace sh1928kd.FizzBuzzProfessionalEdition.Core
 {
-    public class FizzBuzzStyle
+    public static class FizzBuzzStyle
     {
-        public FizzBuzzStyle()
+        public static void BuildSimpleFizzBuzzRule(FizzBuzzDelegate fizzbuzz)
         {
+            AddRule(fizzbuzz.Interactor, 1, n => n % 15u == 0 ? "FizzBuzz" : null);
+            AddRule(fizzbuzz.Interactor, 2, n => n % 3u == 0 ? "Fizz" : null);
+            AddRule(fizzbuzz.Interactor, 3, n => n % 5u == 0 ? "Buzz" : null);
+        }
+
+        private static void AddRule(IFizzBuzzInteractor interactor, uint priority, Func<uint, string> rule)
+        {
+            interactor.Handle(new PriorityFizzBuzzRule(priority, new FizzBuzzRule(rule)));
         }
     }
 }
