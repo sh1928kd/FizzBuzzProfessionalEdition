522e609eeb6d5e8518a1fd22961991a8ad7439ce
sh1928kd <sh1928kd@sh1928kds-MacBook-Pro.local>
2020-05-07 00:56:00 +0900

    :hammer: FizzBuzzDelegateクラスを実装

--- src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzDelegate.cs
+++ src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzDelegate.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using sh1928kd.FizzBuzzProfessionalEdition.Model;
 
 namespace sh1928kd.FizzBuzzProfessionalEdition.Core
 {
     public class FizzBuzzDelegate
     {
-        public FizzBuzzDelegate()
+        private FizzBuzzConverter Converter { get; } = new FizzBuzzConverter();
+        public IFizzBuzzInteractor Interactor { get { return Converter; } }
+
+        public List<string> GenerateOneFor(uint max)
         {
+            return GenerateForEach(new List<uint>(Enumerable.Range(1, (int)max).Select(x => (uint)x)));
+        }
+
+        public List<string> GenerateForEach(List<uint> list)
+        {
+            return list.Select(number => Converter.Convert(number)).ToList();
         }
     }
 }
--- test/sh1928kd.FizzBuzzProfessionalEdition.Core.Tests/FizzBuzzDelegateTest.cs
+++ test/sh1928kd.FizzBuzzProfessionalEdition.Core.Tests/FizzBuzzDelegateTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace sh1928kd.FizzBuzzProfessionalEdition.Core.Tests
@@ -5,28 +6,35 @@ namespace sh1928kd.FizzBuzzProfessionalEdition.Core.Tests
     [TestClass]
     public class FizzBuzzDelegateTest
     {
-        [DataTestMethod]
-        [DataRow()]
-        [TestCategory("TestMethod()")]
-        public void TestMethod_PositivePattern()
+        private static IEnumerable<object[]> TestData4VanillaRule()
         {
-            Assert.Fail();
+            yield return new object[] { 3u, new List<string> { "1", "2", "3" } };
+            yield return new object[] { 10u, new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" } };
         }
 
         [DataTestMethod]
-        [DataRow()]
-        [TestCategory("TestMethod()")]
-        public void TestMethod_AlternativePattern()
+        [DynamicData(nameof(TestData4VanillaRule), DynamicDataSourceType.Method)]
+        [TestCategory("GenerateOneFor()")]
+        public void GenerateOneFor_VanillaRule(uint input, List<string> except)
         {
-            Assert.Fail();
+            var target = new FizzBuzzDelegate();
+            target.GenerateOneFor(input).Is(except);
+        }
+
+        private static IEnumerable<object[]> TestData4MultiplesOfThreeRule()
+        {
+            yield return new object[] { 3u, new List<string> { "1", "2", "Fizz" } };
+            yield return new object[] { 10u, new List<string> { "1", "2", "Fizz", "4", "5", "Fizz", "7", "8", "Fizz", "10" } };
         }
 
         [DataTestMethod]
-        [DataRow()]
-        [TestCategory("TestMethod()")]
-        public void TestMethod_NegativesPattern()
+        [DynamicData(nameof(TestData4MultiplesOfThreeRule), DynamicDataSourceType.Method)]
+        [TestCategory("GenerateOneFor()")]
+        public void GenerateOneFor_MultiplesOfThreeRule(uint input, List<string> except)
         {
-            Assert.Fail();
+            var target = new FizzBuzzDelegate();
+            target.Interactor.Handle(new Model.PriorityFizzBuzzRule(1, new Model.FizzBuzzRule(n => n % 3u == 0 ? "Fizz" : null)));
+            target.GenerateOneFor(input).Is(except);
         }
     }
 }
