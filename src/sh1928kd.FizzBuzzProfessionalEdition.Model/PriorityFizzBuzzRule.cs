4b1f65707856eb020bf818f31a4876e767b90bcd
sh1928kd <sh1928kd@sh1928kds-MacBook-Pro.local>
2020-05-06 23:06:51 +0900

    :hammer: PriorityFizzBuzzRuleクラスを実装

---
+++ src/sh1928kd.FizzBuzzProfessionalEdition.Model/PriorityFizzBuzzRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sh1928kd.FizzBuzzProfessionalEdition.Model
+{
+    public class PriorityFizzBuzzRule
+    {
+        public uint Priority { get; }
+        public FizzBuzzRule Rule { get; }
+
+        public PriorityFizzBuzzRule(uint priority, FizzBuzzRule rule)
+        {
+            Priority = priority;
+            Rule = rule;
+        }
+    }
+}
