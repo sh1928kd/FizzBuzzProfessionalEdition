c488a1a84a5ebe63d640d84c31d1ae85aa478624
sh1928kd <sh1928kd@sh1928kds-MacBook-Pro.local>
2020-05-07 01:00:48 +0900

    :art: 数字リテラルに適切なサフィックスを付与

--- src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzStyle.cs
+++ src/sh1928kd.FizzBuzzProfessionalEdition.Core/FizzBuzzStyle.cs
@@ -7,9 +7,9 @@ namespace sh1928kd.FizzBuzzProfessionalEdition.Core
     {
         public static void BuildSimpleFizzBuzzRule(FizzBuzzDelegate fizzbuzz)
         {
-            AddRule(fizzbuzz.Interactor, 1, n => n % 15u == 0 ? "FizzBuzz" : null);
-            AddRule(fizzbuzz.Interactor, 2, n => n % 3u == 0 ? "Fizz" : null);
-            AddRule(fizzbuzz.Interactor, 3, n => n % 5u == 0 ? "Buzz" : null);
+            AddRule(fizzbuzz.Interactor, 1u, n => n % 15u == 0 ? "FizzBuzz" : null);
+            AddRule(fizzbuzz.Interactor, 2u, n => n % 3u == 0 ? "Fizz" : null);
+            AddRule(fizzbuzz.Interactor, 3u, n => n % 5u == 0 ? "Buzz" : null);
         }
 
         private static void AddRule(IFizzBuzzInteractor interactor, uint priority, Func<uint, string> rule)
